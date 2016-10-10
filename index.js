d316cf88a8fd66bcf75d60c2097555bf0aa43323
psilotec <scottadevito@gmail.com>
2016-10-10 13:20:50 -0400

    Decide data structure - first attempt
    Torn between this data structure or array of questions as objects with
    properties: strand_name, standard_name, question_id

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 /**
  * Created by Scott on 10/10/2016.
  */
+//https://github.com/psilotec/NoRedInk-takehome
 
-// Import question data
+
+function questions () {
+    // Import question data
+    var taxonimy = [
+        ["nouns", "verbs"],
+        ["common nouns", "abstract nouns", "proper nouns", "action verbs", "transitive verbs", "reflexive verbs"],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    ];
+
 
 
 // Import usage data
@@ -10,10 +19,15 @@
 
 
 // Student is prompted for the number of questions they would like to answer
+    // create function numQuestions
     // (must be > 0)
+
 
 // Based on numQuestions, decide how many strands to give
     //solve first
+    function decideQuestions () {
+
+    }
 
 // Prefer questions that have not been assigned yet
     // create variable "hasBeenAssigned"
@@ -23,5 +37,7 @@
 // Use an equal number of standards as possible (6 types: common nouns, abstract nouns, proper nouns, action verbs, transitive verbs, reflexive verbs)
 
 
-
 // Return list of question_ids
+
+
+}
