cce86962d10a454dba4b812f7c20167b81a1a2cf
psilotec <scottadevito@gmail.com>
2016-10-10 12:53:46 -0400

    Create initial algo

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Scott on 10/10/2016.
+ */
+
+// Import question data
+
+
+// Import usage data
+    // Will come back to this if enough time
+
+
+// Student is prompted for the number of questions they would like to answer
+    // (must be > 0)
+
+// Based on numQuestions, decide how many strands to give
+    //solve first
+
+// Prefer questions that have not been assigned yet
+    // create variable "hasBeenAssigned"
+
+
+// Use an equal number of strands for the number of questions (2 types: nouns, verbs)
+// Use an equal number of standards as possible (6 types: common nouns, abstract nouns, proper nouns, action verbs, transitive verbs, reflexive verbs)
+
+
+
+// Return list of question_ids
